bbd9b76841d4b3d98be40778a2d215866bc4d17e
Luis Saiete <luistsaiete@gmail.com>
2023-10-08 23:12:48 +0200

    get init data

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
-WebApp.requestContact((res) => console.log('response', res));
-WebApp.onEvent('contactRequested', (cont) => console.log('contactRequested', cont));
-console.log('window');
-console.log(window.Telegram.WebApp);
-
-// console.log(WebApp.WebAppUser);
+console.log(WebApp.initData);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
