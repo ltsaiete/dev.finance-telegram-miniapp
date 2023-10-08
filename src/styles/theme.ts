eb8ed838ac75b99fec19756d2798530b96077357
Luis Saiete <luistsaiete@gmail.com>
2023-10-08 03:29:06 +0200

    Swith themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,29 @@
 import { DefaultTheme } from 'styled-components/dist/types';
+import WebApp from '@twa-dev/sdk';
+
+interface ThemeProps {
+	main: string;
+	mainDark: string;
+}
+const lightTheme = {
+	main: '#49AA26',
+	mainDark: '#2D4A22'
+};
+const darkTheme = {
+	main: '#8257E5',
+	mainDark: '#28203E'
+};
+const themeColors: ThemeProps = WebApp.colorScheme === 'dark' ? darkTheme : lightTheme;
 
 export const theme: DefaultTheme = {
 	colors: {
-		main: '#49AA26',
-		mainDark: '#2D4A22',
-		textBase: '#969CB2',
-		textTitle: '#363F5F',
-		background: '#F0F2F5',
-		boxFill: '#FFFFFF',
+		textBase: WebApp.themeParams.hint_color,
+		textTitle: WebApp.themeParams.text_color,
+		background: WebApp.themeParams.secondary_bg_color,
+		boxFill: WebApp.themeParams.bg_color,
 		green: '#49AA26',
 		red: '#E92929',
-		white: '#FFF'
+		white: '#FFF',
+		...themeColors
 	}
 };
